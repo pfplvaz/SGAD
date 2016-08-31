b2272fe9229425376d874c00f70cd7a64f1a7c0b
pfplvaz <pfplvaz@gmail.com>
2016-08-31 16:58:52 -0300

    Criação das classes exceptions restantes

---
+++ src/main/br/uefs/sgad/controller/NaoEhPastaException.java
@@ -0,0 +1,20 @@
+package main.br.uefs.sgad.controller;
+
+public class NaoEhPastaException extends Exception{
+
+	/**
+	 * Necessário para a serialização da exception.
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Construtor passa a mensagem de erro para a classe Throwable, que imprimira-la na tela.
+	 * @param msg
+	 */
+	
+	public NaoEhPastaException(String msg) {
+		super(msg);
+	}
+	
+
+}
